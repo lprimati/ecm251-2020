e9d533dae1960a2ee064a07a82e3b49ad649de8d
lprimati <37450426+lprimati@users.noreply.github.com>
2020-04-05 14:40:50 -0300

    Add files via upload
    agora foi

---
+++ ecm251-2020/Pratica01/Aula/MeuPrograma.java
@@ -0,0 +1,7 @@
+public class MeuPrograma {
+	public static void main(String[] args){
+		
+		// Mensagem de saida do sistema
+		System.out.println("Minha primeira aplicaçao Java!!");
+	}
+}
---
+++ ecm251-2020/Pratica01/Aula/TesteVSCode.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class TesteVSCode{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ola mundo!");
+        String nome;
+        nome = scanner.nextLine();
+        System.out.println("nome");
+    }
+}
